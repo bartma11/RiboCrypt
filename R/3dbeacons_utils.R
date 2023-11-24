5316a5a2735ca7971f739499f7d951f50ab99f59
Donut <bartek.henkiel@gmail.com>
2023-11-24 17:12:14 +0000

    Modularized fetching data from 3dbeacons. Added comments with documentation

--- R/3dbeacons_utils.R
+++ R/3dbeacons_utils.R
@@ -1,5 +1,15 @@
+# 3d-beacons api description
+# https://www.ebi.ac.uk/pdbe/pdbe-kb/3dbeacons/api
+
 beacon_main_api_url <- function() {
   'https://www.ebi.ac.uk/pdbe/pdbe-kb/3dbeacons/api/uniprot/summary/'
+}
+
+valid_providers <- function() {
+  c("pdbe", "ped", "swissmodel",
+    "alphafold", "sasbdb", "alphafill",
+    "hegelab", "modelarchive", "isoformio", 
+    "levylab", "all")
 }
 
 #' Fetch summary of uniprot id
@@ -8,53 +18,51 @@ beacon_main_api_url <- function() {
 #' @return a character of json
 #' @importFrom RCurl getURI
 #' @importFrom jsonlite fromJSON
-fetch_summary <- function(qualifier, provider = "alphafold") {
+fetch_summary <- function(qualifier, provider = "all") {
   # browser()
   provider_url <-
   if (provider == "all") {
-    stop("This does not work yet!")
     ""
   } else paste0("?provider=", provider)
 
-  stringr::str_interp(
-    paste0(beacon_main_api_url(), "${qualifier}", ".json", provider_url),
-    list(qualifier = qualifier)
-  ) %>% RCurl::getURI() %>% jsonlite::fromJSON()
+  paste0(beacon_main_api_url(), qualifier, ".json", provider_url) %>% 
+    RCurl::getURI() %>% 
+    jsonlite::fromJSON()
 }
 
-model_identifiers_from_summary <- function(data) {
-  data$model_identifiers
+#' @param summ 3dbeacons summary for a UniProt id
+#' @return vector of strings representing model ids 
+model_identifiers_from_summary <- function(summ) {
+  summ$structures$summary$model_identifier
 }
 
-model_categories_from_summary <- function(data) {
-  data$model_category
+#' @param summ 3dbeacons summary for a UniProt id
+#' @return vector of strings representing model categories
+model_categories_from_summary <- function(summ) {
+  summ$structures$summary$model_category
 }
 
-model_urls_from_summary <- function(data) {
-  data$structures$summary$model_url
+#' @param summ 3dbeacons summary for a UniProt id
+#' @return vector of strings representing urls of model files
+model_urls_from_summary <- function(summ) {
+  summ$structures$summary$model_url
 }
 
-model_formats_from_summary <- function(data) {
-  data$model_format
+#' @param summ 3dbeacons summary for a UniProt id
+#' @return vector of strings representing file extensions of model files
+model_formats_from_summary <- function(summ) {
+  summ$structures$summary$model_format
 }
 
-entity_identifiers_from_summary <- function(data) {
-  mapply(function (x) {
-    x$identifier
-  },
-  data$structures$summary$entities)
-}
-
-entity_identifier_categories_from_summary <- function(data) {
-  mapply(function (x) {
-    x$identifier_category
-  },
-  data$structures$summary$entities)
-}
-
-chain_ids_from_summary <- function(data) {
-  mapply(function (x) {
-    x$chain_ids
-  },
-  data$structures$summary$entities)
+#' Fetch 3dbeacons data about models for a UniProt id
+#' @param qualifier UniProt id
+#' @param provider optional, "all" by default, for available provides see valid_providers in this file
+#' @return a data table where each row contains id, category, url and file format of a model
+fetch_models_for_structure <- function(qualifier, provider = "all") {
+  summ <- fetch_summary(qualifier, provider)
+  model_id <- model_identifiers_from_summary(summ)
+  model_category <- model_categories_from_summary(summ)
+  model_url <- model_urls_from_summary(summ)
+  model_format <- model_formats_from_summary(summ)
+  data.table(model_id, model_category, model_url, model_format)
 }
